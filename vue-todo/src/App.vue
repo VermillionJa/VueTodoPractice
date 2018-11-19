8e1a5f5f62d1b2b4b6821730cc7940f11b2d3c3e
Jake Vermillion <jacob.a.vermillion@gmail.com>
2018-11-18 20:08:46 -0700

    Added base structure and styles to the App.vue

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <h1 class="title">Todos</h1>
   </div>
 </template>
 
@@ -11,6 +12,21 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+* {
+  margin-top: 0;
+}
 
+#app {
+  margin: auto;
+  width: 40%;
+}
+
+.title {
+  text-align: center;
+  font-size: 5rem;
+  margin-top: 1rem;
+  color: rgba(170, 55, 55, 0.192);
+  text-transform: lowercase;
+}
 </style>
